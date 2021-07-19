f554ea69cececfe8db164f064925e0e9a0145af5
easylearningbd <elebdsoft@gmail.com>
2021-07-19 06:37:04 +0600

    12 Managing Transition and Appearing Animation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/assets/css/custom.css';
 import '../src/assets/css/fontawesome.css';
+import '../src/assets/css/animate.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
