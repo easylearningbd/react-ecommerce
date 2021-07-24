0a301273544f8f2e94dc60e886b677206a4f06dd
easylearningbd <elebdsoft@gmail.com>
2021-07-24 20:30:28 +0600

    24 Nav Menu Desktop Part 2

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react'
-import {Navbar,Container, Row, Col} from 'react-bootstrap';
+import {Navbar,Container, Row, Col,Button} from 'react-bootstrap';
 import Logo from '../../assets/images/easyshop.png';
 import {Link} from "react-router-dom";
+ 
    
  class NavMenuDesktop extends Component {
      render() {
@@ -10,16 +11,27 @@ import {Link} from "react-router-dom";
 <div className="TopSectionDown">
 <Navbar fixed={"top"} className="navbar" bg="light">
 
-    <Container fluid={"true"}>
+    <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
          <Row>
               <Col lg={4} md={4} sm={12} xs={12}>
               <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
               </Col>
 
-              <Col lg={4} md={4} sm={12} xs={12}>
+              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                   <div className="input-group w-100">
+                   <input type="text" className="form-control" />
+                   <Button type="button" className="btn site-btn"><i className="fa fa-search"> </i> 
+                   </Button>
+                   </div>
               </Col>
 
-              <Col lg={4} md={4} sm={12} xs={12}>
+              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                   <Link to="/" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>                  
+                   </Link>
+                   <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
+                   <Link to="/" className="h4 btn">LOGIN</Link>
+                   
+                   <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
               </Col>
 
          </Row>
