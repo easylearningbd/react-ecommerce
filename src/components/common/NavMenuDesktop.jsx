ccfae58d325546f943343fd8eb9a5e1d90c75607
easylearningbd <elebdsoft@gmail.com>
2021-07-24 19:47:47 +0600

    23 Nav Menu Desktop Part 1

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react'
+import {Navbar,Container, Row, Col} from 'react-bootstrap';
+import Logo from '../../assets/images/easyshop.png';
+import {Link} from "react-router-dom";
+   
  class NavMenuDesktop extends Component {
      render() {
           return (
-               <div>
-                    
-               </div>
+               <Fragment>
+<div className="TopSectionDown">
+<Navbar fixed={"top"} className="navbar" bg="light">
+
+    <Container fluid={"true"}>
+         <Row>
+              <Col lg={4} md={4} sm={12} xs={12}>
+              <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
+              </Col>
+
+              <Col lg={4} md={4} sm={12} xs={12}>
+              </Col>
+
+              <Col lg={4} md={4} sm={12} xs={12}>
+              </Col>
+
+         </Row>
+   
+    </Container>
+
+  </Navbar>
+  </div>
+
+               </Fragment>
           )
      }
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
@@ -7,13 +8,15 @@ import NewArrival from '../components/home/NewArrival'
 
 export class HomePage extends Component {
      render() {
-          return (
-               <Fragment>
-                    <HomeTop />
+          return (   
+               <Fragment> 
+                    <NavMenuDesktop />       
+                    <HomeTop />  
                     <FeaturedProducts />
                     <NewArrival />
-                    <Collection />
                     <Categories />
+                    <Collection />
+                    
                </Fragment>
           )
      }
