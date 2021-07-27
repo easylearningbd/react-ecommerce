a014b5dbc5ce14338b5659aacdc9ca9ce89bdbad
easylearningbd <elebdsoft@gmail.com>
2021-07-28 01:37:37 +0600

    45 Cart List Part 2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -22,7 +22,7 @@ class Cart extends Component {
                <Card.Body>
                <Row>
                     <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="w-100 h-100" src={Product1} />
+                         <img className="cart-product-img" src={Product1} />
                     </Col>
 
                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -49,7 +49,7 @@ class Cart extends Component {
                <Card.Body>
                <Row>
                     <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="w-100 h-100" src={Product1} />
+                         <img className="cart-product-img" src={Product1} />
                     </Col>
 
                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -75,7 +75,7 @@ class Cart extends Component {
                <Card.Body>
                <Row>
                     <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="w-100 h-100" src={Product1} />
+                         <img className="cart-product-img" src={Product1} />
                     </Col>
 
                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -102,7 +102,7 @@ class Cart extends Component {
                <Card.Body>
                <Row>
                     <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="w-100 h-100" src={Product1} />
+                         <img className="cart-product-img" src="https://rukminim1.flixcart.com/image/416/416/knm2s280/mobile/j/x/c/hot-10-play-x688b-infinix-original-imag29gxqzuxkmnk.jpeg?q=70" />
                     </Col>
 
                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -123,6 +123,25 @@ class Cart extends Component {
 
 
 
+
+
+
+     <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+          <Card >                
+               <Card.Body>
+               <Row>
+                     
+
+     <Col md={4} lg={4} sm={6} xs={6}>
+         
+                <h5> Total Item = 05 </h5>
+                <h5>Total Price = 5000$</h5>
+                <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-shopping-cart"></i> CheckOut </Button>
+                    </Col> 
+               </Row>              
+               </Card.Body>               
+          </Card>
+     </Col> 
 
 
 
