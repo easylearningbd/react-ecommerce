aad3e471098b90f56f61e4fa3555c432e5794c9d
easylearningbd <elebdsoft@gmail.com>
2021-07-29 03:54:35 +0600

    50 Consume Contact Rest API From Client Side Part 1

--- src/components/common/Contact.jsx
+++ src/components/common/Contact.jsx
@@ -3,6 +3,41 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
  
 
 export class Contact extends Component {
+
+     constructor(){
+          super();
+          this.state={
+               name:"",
+               email:"",
+               message:""
+          }
+     }
+
+          nameOnChange =(event)=>{
+               let name = event.target.value;
+               // alert(name);
+               this.setState({name:name})
+          }
+
+          emailOnChange =(event)=>{
+               let email = event.target.value;
+               // alert(email);
+               this.setState({email:email})
+          }
+
+          messageOnChange =(event)=>{
+               let message = event.target.value;
+               // alert(message);
+               this.setState({message:message})
+          }
+
+          onFormSubmit =(event)=>{
+               alert("Hello hi");
+               event.prventDefault();
+
+          }
+
+
      render() {
           return (
                <Fragment>
@@ -12,15 +47,17 @@ export class Contact extends Component {
      
                     <Row className="text-center">
              <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-     <Form className="onboardForm">
+     <Form onSubmit={this.onFormSubmit} className="onboardForm">
           <h4 className="section-title-login">CONTACT WITH US </h4>
           <h6 className="section-sub-title">Please Contact With Us </h6>
-          <input className="form-control m-2" type="text" placeholder="Enter Mobile Number" />
+          
+          <input onChange={this.nameOnChange} className="form-control m-2" type="text" placeholder="Enter Your Name" />
 
-          <input className="form-control m-2" type="email" placeholder="Enter Email" />
+          <input onChange={this.emailOnChange} className="form-control m-2" type="email" placeholder="Enter Email" />
 
-          <input className="form-control m-2" type="text" placeholder="Enter Your Message" />
-          <Button className="btn btn-block m-2 site-btn-login"> Send </Button>
+          <Form.Control onChange={this.messageOnChange} className="form-control m-2" as="textarea" rows={3} placeholder="Message" />
+
+          <Button type="submit" className="btn btn-block m-2 site-btn-login"> Send </Button>
           
      </Form>
      
