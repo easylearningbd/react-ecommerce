b8367eab1d153662a38b1a4724669cc0df9f0828
easylearningbd <elebdsoft@gmail.com>
2021-07-31 02:18:11 +0600

    56 Consume Site Info API in Client Side Part 1

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -2,6 +2,7 @@ class AppURL {
      static BaseURL = "http://127.0.0.1:8000/api"
      static VisitorDetails = this.BaseURL+"/getvisitor"
      static PostContact = this.BaseURL+"/postcontact"
+     static AllSiteInfo = this.BaseURL+"/allsiteinfo"
       
 }
 
