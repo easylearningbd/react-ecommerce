174da50a9183599053f73f319974aeb37a624566
easylearningbd <elebdsoft@gmail.com>
2021-07-31 04:05:28 +0600

    61 Consume Site Info API in Client Side Part 6

--- src/components/common/FooterDesktop.jsx
+++ src/components/common/FooterDesktop.jsx
@@ -3,8 +3,50 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Apple from '../../assets/images/apple.png'
 import Google from '../../assets/images/google.png'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
+import ReactHtmlParser from 'react-html-parser';
 
 export class FooterDesktop extends Component {
+     constructor(){
+          super();
+          this.state={
+               address:"",
+               android_app_link:"",
+               ios_app_link:"",
+               facbook_link:"",
+               twitter_link:"",
+               instagram_link:"",
+               copyright_text:"", 
+               loaderDiv:"",
+               mainDiv:"d-none"
+          }
+     }
+
+     componentDidMount(){
+          axios.get(AppURL.AllSiteInfo).then(response =>{
+               let StatusCode = response.status;
+               if(StatusCode==200){
+                    let JsonData = (response.data)[0];
+                    this.setState({
+                         address:JsonData['address'],
+                         android_app_link:JsonData['android_app_link'],
+                         ios_app_link:JsonData['ios_app_link'],
+                         facbook_link:JsonData['facbook_link'],
+                         twitter_link:JsonData['twitter_link'],
+                         instagram_link:JsonData['instagram_link'],
+                         copyright_text:JsonData['copyright_text'], 
+                         loaderDiv:"d-none",
+                         mainDiv:""
+                    });
+               } 
+
+          }).catch(error=>{
+
+          });
+     }
+
+
      render() {
           return (
                <Fragment>
@@ -12,14 +54,38 @@ export class FooterDesktop extends Component {
                     <Container>
      <Row className="px-0 my-5">
           <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
+               
+          <div className={this.state.loaderDiv}>
+
+                         <div class="ph-item">
+                         <div class="ph-col-12">        
+                         <div class="ph-row">           
+                         <div class="ph-col-4"></div>
+                         <div class="ph-col-8 empty"></div>
+                         <div class="ph-col-6"></div>
+                         <div class="ph-col-6 empty"></div>
+                         <div class="ph-col-12"></div>
+                         <div class="ph-col-12"></div>
+                         
+                         </div>
+                         </div>
+                         </div>
+
+
+</div>
+
+          <div className={this.state.mainDiv}>
                <h5 className="footer-menu-title">OFFICE ADDRESS</h5>
-               <p>1635 Franklin Street Montgomery, Near Sherwood Mall. AL 36104 <br></br>
-               Email: Support@easylearningbd.com
-               </p>
+
+                 { ReactHtmlParser(this.state.address) } 
+               </div>
+               
                <h5 className="footer-menu-title">SOCIAL LINK</h5>
-               <a href=""><i className="fab m-1 h4 fa-facebook"></i></a>
-               <a href=""><i className="fab m-1 h4 fa-instagram"></i></a>
-               <a href=""><i className="fab m-1 h4 fa-twitter"></i></a>
+               <a href={this.state.facbook_link} target="_blank"><i className="fab m-1 h4 fa-facebook"></i></a>
+               
+               <a href={this.state.instagram_link} target="_blank"><i className="fab m-1 h4 fa-instagram"></i></a>
+
+               <a href={this.state.twitter_link} target="_blank"><i className="fab m-1 h4 fa-twitter"></i></a>
           </Col>
 
           <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
@@ -38,8 +104,9 @@ export class FooterDesktop extends Component {
 
           <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
           <h5 className="footer-menu-title">DOWNLOAD APPS</h5>
-          <a><img src={Google}  /></a><br></br>
-          <a><img className="mt-2" src={Apple}  /></a><br></br>
+          <a href={this.state.android_app_link} target="_blank"><img src={Google}  /></a><br></br>
+          
+          <a href={this.state.ios_app_link} target="_blank"><img className="mt-2" src={Apple}  /></a><br></br>
           Change Your Language <br></br>
           <div id="google_translate_element">  </div>
           </Col>
@@ -50,7 +117,7 @@ export class FooterDesktop extends Component {
      <Container fluid={true} className="text-center m-0 pt-3 pb-1 bg-dark">
                     <Container>
                          <Row>
-       <h6 className="text-white">© Copyright 2021 by easy Shop, All Rights Reserved</h6>
+       <h6 className="text-white"> { ReactHtmlParser(this.state.copyright_text) }  </h6>
                          </Row>
                     </Container>
                </Container> 
