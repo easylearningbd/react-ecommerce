81fb02684e7ccc1a6936e806a32df141fcf3a3b2
easylearningbd <elebdsoft@gmail.com>
2021-07-31 03:03:43 +0600

    58 Consume Site Info API in Client Side Part 3

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -3,6 +3,8 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
 import ReactHtmlParser from 'react-html-parser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class Purchase extends Component {
      constructor(){
@@ -13,17 +15,38 @@ export class Purchase extends Component {
      }
 
      componentDidMount(){
-          axios.get(AppURL.AllSiteInfo).then(response =>{
-               let StatusCode = response.status;
-               if(StatusCode==200){
-                    let JsonData = (response.data)[0]['parchase_guide'];
-                    this.setState({purchase:JsonData});
-               } 
 
+          let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
+
+          if(SiteInfoPurchase == null){
+
+               axios.get(AppURL.AllSiteInfo).then(response =>{
+                    let StatusCode = response.status;
+                    if(StatusCode==200){
+                         let JsonData = (response.data)[0]['parchase_guide'];
+                         this.setState({purchase:JsonData});
+
+                         sessionStorage.setItem("SiteInfoPurchase",JsonData)
+                    } 
+                    else{
+                         toast.error("Somthing Went Wrong",{
+                              position: "bottom-center"
+                         });
+                    }
+      
+         
           }).catch(error=>{
-
+               toast.error("Somthing Went Wrong",{
+                    position: "bottom-center"
+               });
           });
+
+     }  // end If Conditon 
+     else{
+          this.setState({purchase:SiteInfoPurchase});
      }
+
+     } 
 
      render() {
           return (
@@ -40,6 +63,7 @@ export class Purchase extends Component {
                          </Col>
                     </Row>
                </Container>
+               <ToastContainer />
           </Fragment>
           )
      }
