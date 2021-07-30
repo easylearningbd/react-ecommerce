3af5ae7c3fa385c7551e51f5dcd37c028bf80bf2
easylearningbd <elebdsoft@gmail.com>
2021-07-31 03:43:35 +0600

    60 Consume Site Info API in Client Side Part 5

--- src/components/others/Privacy.jsx
+++ src/components/others/Privacy.jsx
@@ -1,23 +1,90 @@
 import React, { Component, Fragment } from 'react'
 import { Container,Row,Col, Form,Button } from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
+import ReactHtmlParser from 'react-html-parser';
 
 class Privacy extends Component {
-     render() {
+     constructor(){
+          super();
+          this.state={
+               privacy:"",
+               loaderDiv:"",
+               mainDiv:"d-none"
+          }
+     }
+
+     componentDidMount(){
+          axios.get(AppURL.AllSiteInfo).then(response =>{
+               let StatusCode = response.status;
+               if(StatusCode==200){
+                    let JsonData = (response.data)[0]['privacy'];
+                    this.setState({privacy:JsonData,loaderDiv:"d-none",mainDiv:""});
+               } 
+
+          }).catch(error=>{
+
+          });
+     }
+
+
+
+     render() { 
           return (
                <Fragment>
                <Container>
                     <Row className="p-2">
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-      <h4 className="section-title-login">Privacy Page </h4>
+
+
+            <div className={this.state.loaderDiv}>
+
+<div class="ph-item">
+<div class="ph-col-12">        
+<div class="ph-row">           
+<div class="ph-col-4"></div>
+<div class="ph-col-8 empty"></div>
+<div class="ph-col-6"></div>
+<div class="ph-col-6 empty"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+</div>
+</div>
+</div>
+
+
+<div class="ph-item">
+<div class="ph-col-12">        
+<div class="ph-row">           
+<div class="ph-col-4"></div>
+<div class="ph-col-8 empty"></div>
+<div class="ph-col-6"></div>
+<div class="ph-col-6 empty"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+<div class="ph-col-12"></div>
+</div>
+</div>
+</div>
+
+
+
+</div>
+
+ 
+
+
+<div className={this.state.mainDiv}>
+      <h4 className="section-title-login">Privacy Page</h4>
       <p className="section-title-contact">
-      Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.<br></br><br></br>
 
-I am working online for the last 7 years and have created several successful websites running on the internet. I try to create a project-based course that helps you to learn professionally and make you fell as a complete developer. easy learning exists to help you succeed in life.<br></br><br></br>
-
-Each course has been hand-tailored to teach a specific skill. I hope you agree! Whether you’re trying to learn a new skill from scratch or want to refresh your memory on something you’ve learned in the past, you’ve come to the right place.<br></br>
-
-Education makes the world a better place. Make your world better with new skills.
+      { ReactHtmlParser(this.state.privacy) }       
       </p>
+
+      </div>
      
      
                          </Col>
