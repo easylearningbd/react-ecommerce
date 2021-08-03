f394c7fa2f942788be0860f9034177d356a2065c
easylearningbd <elebdsoft@gmail.com>
2021-08-04 03:54:04 +0600

    72  Consume Product List API Part 3

--- src/components/home/NewArrival.jsx
+++ src/components/home/NewArrival.jsx
@@ -3,11 +3,16 @@ import {Container,Row,Col,Card} from 'react-bootstrap'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
 class NewArrival extends Component {
 
      constructor(props){
           super(props);
+          this.state={
+               ProductData:[]
+          }
           this.next=this.next.bind(this);
           this.previous=this.previous.bind(this)
      }
@@ -19,8 +24,53 @@ class NewArrival extends Component {
      }
 
 
+     componentDidMount(){
+          axios.get(AppURL.ProductListByRemark("NEW")).then(response =>{
+               
+               this.setState({ProductData:response.data});         
+
+          }).catch(error=>{
+
+          });
+     } 
+
 
      render() {
+
+          const NewList = this.state.ProductData;
+          const MyView = NewList.map((NewList,i)=>{
+
+          if(NewList.special_price=="na"){
+               return    <div>
+               <Card className="image-box card">
+               <img className="center" src={NewList.image} />   
+               <Card.Body> 
+               <p className="product-name-on-card">{NewList.title}</p>
+               <p className="product-price-on-card">Price : ${NewList.price}</p>
+                    
+               </Card.Body>
+               </Card>
+               </div>
+
+          }
+          else{
+
+               return    <div>
+               <Card className="image-box card">
+               <img className="center" src={NewList.image} />   
+               <Card.Body> 
+               <p className="product-name-on-card">{NewList.title}</p>
+               <p className="product-price-on-card">Price : <strike className="text-secondary">${NewList.price}</strike> ${NewList.special_price}</p>
+                    
+               </Card.Body>
+               </Card>
+               </div>
+
+          } 
+ 
+          });
+
+
           var settings = {
                dots: false,
                infinite: true,
@@ -76,86 +126,8 @@ class NewArrival extends Component {
                <Row>
 
                <Slider ref={c=>(this.slider=c)} {...settings}>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/k7z3afk0/watch/t/c/x/lcs-8188-lois-caron-original-imafq3k9ztzdkyhe.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jeka07k0/watch/4/p/y/38024pp25-fastrack-original-imaf37n5df3bn52n.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/kka1si80/watch/4/t/k/tw-02524-teenager-luxurious-fashion-silicone-black-colored-led-original-imafznht7bzfmj7d.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jw0zr0w0/watch/c/u/r/ls2811-limestone-original-imafgrxqf8qvecjd.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/kpodocw0/watch/p/l/t/anlg-428-blue-blu-analogue-original-imag3uxbhfkyhahf.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/k48rwcw0/watch/k/v/f/lcs-8190-lois-caron-original-imafn7fsyttnpybp.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
-          <div>
-          <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/ke353m80-0/watch/e/b/s/fresh-new-arrival-latest-men-watch-watches-men-ghadi-gents-boys-original-imafuupqgaanhtxu.jpeg?q=50" />   
-          <Card.Body> 
-          <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-          <p className="product-price-on-card">Price : $120</p>
-               
-          </Card.Body>
-          </Card>
-          </div>
+          {MyView}
+           
         </Slider>
 
                </Row>
