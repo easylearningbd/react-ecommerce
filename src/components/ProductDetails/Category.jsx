1d75d08febf0587621812860db05add418c06284
easylearningbd <elebdsoft@gmail.com>
2021-08-04 05:40:16 +0600

    74  Consume Product List API Part 1

--- src/components/ProductDetails/Category.jsx
+++ src/components/ProductDetails/Category.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
 import { Fragment } from 'react'
+import {Container,Row,Col,Card} from 'react-bootstrap'
 
 class Category extends Component {
      render() {
+
+          const MyList = this.props.ProductData;
+          const Category = this.props.Category;
+          const MyView = MyList.map((ProductList,i)=>{
+
+          if(ProductList.special_price=="na"){
+               return   <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+               <Card className="image-box card w-100">
+               <img className="center w-75" src={ProductList.image} />   
+               <Card.Body> 
+               <p className="product-name-on-card">{ProductList.title}</p>
+               <p className="product-price-on-card">Price : ${ProductList.price}</p>
+                    
+               </Card.Body>
+               </Card>          
+               </Col>
+
+          }
+          else{
+
+               return   <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+               <Card className="image-box card w-100">
+               <img className="center w-75" src={ProductList.image} />   
+               <Card.Body> 
+               <p className="product-name-on-card">{ProductList.title}</p>
+               <p className="product-price-on-card">Price : <strike className="text-secondary">${ProductList.price}</strike> ${ProductList.special_price}</p>
+                    
+               </Card.Body>
+               </Card>          
+               </Col>
+
+          } 
+ 
+          });
+
+
+
           return (
                <Fragment>
-                    
-               </Fragment>
+               <Container className="text-center" fluid={true}>
+      <div className="section-title text-center mb-55"><h2> {Category }  </h2>
+      
+      </div>
+
+ <Row> 
+           {MyView}
+ </Row>
+               </Container>
+          </Fragment>
           )
      }
 }
