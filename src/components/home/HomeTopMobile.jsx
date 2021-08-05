f4e069fc38b7418837af2862a23086a3a1afe284
easylearningbd <elebdsoft@gmail.com>
2021-08-05 06:03:30 +0600

    79 Consume API For Home Slider for Mobile

--- src/components/home/HomeTopMobile.jsx
+++ src/components/home/HomeTopMobile.jsx
@@ -1,15 +1,35 @@
 import React, { Component,Fragment } from 'react'
 import HomeSlider from './HomeSlider'
 import {Container,Row,Col} from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
 class HomeTopMobile extends Component {
+
+     constructor(){
+          super();
+          this.state ={ 
+               SliderData:[]
+          }
+     }
+
+     componentDidMount(){ 
+          axios.get(AppURL.AllSlider).then(response =>{ 
+               this.setState({SliderData:response.data});
+
+         }).catch(error=>{
+
+         });
+     }
+
+
      render() {
           return (
                <Fragment>
         <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                          <Row className="p-0 m-0 overflow-hidden"> 
                               <Col lg={12} md={12} sm={12}> 
-                              
+                              <HomeSlider data={this.state.SliderData} />
                               </Col>
                          </Row>
                     </Container>
