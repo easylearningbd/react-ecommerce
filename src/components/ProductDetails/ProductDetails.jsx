561678b2ef5782aeaf54695cf17fadd20b9010ce
easylearningbd <elebdsoft@gmail.com>
2021-08-08 06:29:53 +0600

    92 Consume Product Details API Part 6

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -16,6 +16,23 @@ class ProductDetails extends Component {
      }
 
 
+     PriceOption(price,special_price){
+          if(special_price=="na"){
+               return (
+            <p className="product-price-on-card"> Price : {price}$ </p>
+               )
+          }else{
+
+               return (
+                    <p className="product-price-on-card">
+                      Price : <strike className="text-secondary">{price}$ </strike> {special_price}$ 
+                          </p>
+               )
+
+          }
+     }
+
+
      render() {
 
           let ProductAllData = this.props.data;
@@ -100,11 +117,11 @@ class ProductDetails extends Component {
           <Col className="p-3 " md={6} lg={6} sm={12} xs={12}>
           <h5 className="Product-Name"> {title} </h5>
           <h6 className="section-sub-title"> {short_description} </h6>
-          <div className="input-group">
-               <div className="Product-price-card d-inline ">Reguler Price ${price}</div>
-               <div className="Product-price-card d-inline ">50% Discount</div>
-               <div className="Product-price-card d-inline ">New Price ${special_price}</div>
-          </div>
+          
+
+               {this.PriceOption(price,special_price)}
+
+
           <h6 className="mt-2">Category : <b>{category}</b>  </h6>          
 
           <h6 className="mt-2">SubCategory : <b>{subcategory}</b></h6>
