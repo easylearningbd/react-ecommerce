089d752673062e162cff87aa89b4813a59de7472
easylearningbd <elebdsoft@gmail.com>
2021-08-14 06:12:59 +0600

    127 Product Image Zoom Part 2

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -11,12 +11,14 @@ class ProductDetails extends Component {
 
      constructor(){
           super();
+          this.state={
+               previewImg:"0"
+          }
      }
 
-     imgOnClick(event){
+     imgOnClick = (event) => {
           let imgSrc = event.target.getAttribute('src');
-          let previewImg = document.getElementById('previewImg');
-               ReactDOM.findDOMNode(previewImg).setAttribute('src',imgSrc)
+          this.setState({previewImg:imgSrc})
      }
 
 
@@ -45,6 +47,10 @@ class ProductDetails extends Component {
           let category = ProductAllData['productList'][0]['category'];
           let subcategory = ProductAllData['productList'][0]['subcategory'];
           let image = ProductAllData['productList'][0]['image'];
+
+          if(this.state.previewImg === "0"){
+               this.setState({previewImg:image})
+          }
 
           let price = ProductAllData['productList'][0]['price'];
           let product_code = ProductAllData['productList'][0]['product_code'];
@@ -115,10 +121,10 @@ class ProductDetails extends Component {
 <Col className="shadow-sm bg-white pb-3 mt-4" md={12} lg={12} sm={12} xs={12}>
      <Row>
           <Col className="p-3" md={6} lg={6} sm={12} xs={12}>
-          <img id="previewImg" className="bigimage" src={image_one} />
+          {/* <img id="previewImg" className="bigimage" src={image_one} /> */}
 
-               <div className="bigimage">
-          <InnerImageZoom zoomScale={1.8} zoomType={"hover"} src={image_one} zoomSrc={image_one} />
+               <div className="">
+          <InnerImageZoom zoomScale={1.8} zoomType={"hover"} src={this.state.previewImg} zoomSrc={this.state.previewImg} />
           </div>
 
           <Container  className="my-3">
