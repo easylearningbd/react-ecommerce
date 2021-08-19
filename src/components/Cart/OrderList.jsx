d3da7f6754a6253b87f1359fb66962639486dfb7
easylearningbd <elebdsoft@gmail.com>
2021-08-20 03:51:20 +0600

    161 Product Order History Part 3

--- src/components/Cart/OrderList.jsx
+++ src/components/Cart/OrderList.jsx
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 import { Fragment } from 'react'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
-import {Navbar,Container, Row, Col,Button,Card} from 'react-bootstrap';
+import {Navbar,Container, Row, Col,Button,Card,Modal} from 'react-bootstrap';
 export class OrderList extends Component {
      constructor(){
           super();
           this.state={
-               ProductData:[],  
+               ProductData:[],
+               show:false,
+               NotificationData:[],
+               isLoading:"",
+               mainDiv:"d-none",
+               Notificationmsg:"",
+               Notificationtitle:"",
+               Notificationdate:"" 
 
           }
      }
@@ -23,6 +30,21 @@ export class OrderList extends Component {
           });
      } 
 
+
+     handleClose = () =>{
+          this.setState({ show:false})
+      };  
+
+      handleShow = (event) => {
+           this.setState({ show:true });
+           let Nmsg = event.target.getAttribute("data-message");
+           let Ntitle = event.target.getAttribute("data-title");
+           let Ndate = event.target.getAttribute("data-date");
+           this.setState({Notificationmsg:Nmsg,Notificationtitle:Ntitle,Notificationdate:Ndate})
+      }; 
+
+
+
      render() {
 
           const MyList = this.state.ProductData;         
@@ -35,7 +57,7 @@ export class OrderList extends Component {
           <h6>Price = {ProductList.unit_price} x {ProductList.quantity} = {ProductList.total_price}$</h6>
           <h6>Stauts = {ProductList.order_status} </h6>
           </Col>
-          <Button className="btn btn-danger">Post Review </Button>
+          <Button onClick={this.handleShow} className="btn btn-danger">Post Review </Button>
 <hr></hr>
                </div>
 
@@ -63,6 +85,30 @@ export class OrderList extends Component {
 
 
                    </Container>
+
+
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+           <h6><i className="fa fa-bell"></i> Post Your Review     </h6>
+        </Modal.Header>
+        <Modal.Body>
+             <h6>review</h6>
+             <p>
+             review
+             </p> 
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          
+        </Modal.Footer>
+      </Modal>
+
+
+
                </Fragment>
           )
      }
